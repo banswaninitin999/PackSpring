99f2736b5f452c69b3b9d502f983eaaa6f5219bf
nitinb99 <banswaninitin999@gmail.com>
2019-03-09 23:28:12 -0600

    Using Applicaion Context

--- src/Test.java
+++ src/Test.java
@@ -1,14 +1,21 @@
 import org.springframework.beans.factory.BeanFactory;  
-import org.springframework.beans.factory.xml.XmlBeanFactory;  
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;  
 import org.springframework.core.io.Resource;  
   
 public class Test {  
 public static void main(String[] args) {  
-    Resource resource=new ClassPathResource("ApplicationContext.xml");  
-    BeanFactory factory=new XmlBeanFactory(resource);  
-      
-    Student student=(Student)factory.getBean("studentbean");  
+    
+	//Resource resource=new ClassPathResource("ApplicationContext.xml");  
+    //BeanFactory factory=new XmlBeanFactory(resource);  
+   
+    ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+    
+    //Student student=(Student)factory.getBean("studentbean");  
+    
+    Student student=(Student)context.getBean("studentbean");
     student.displayInfo();  //Display method abc
 }  
 }  
