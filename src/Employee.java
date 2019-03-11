2b47727be15bc99528bf13dddf977c5e11929208
nitinb99 <banswaninitin999@gmail.com>
2019-03-10 20:23:21 -0500

    Constructor Injection with Dependent Object - Address

--- src/Employee.java
+++ src/Employee.java
@@ -1,21 +1,22 @@
 
 public class Employee {  
 private int id;  
-private String name;  
+private String name; 
+private Address address;    //Aggregation  
 
 public Employee() {System.out.println("def cons");}  
 
-public Employee(int id) {this.id = id;}  
-
-public Employee(String name) {  this.name = name;}  
-
-public Employee(int id, String name) {  
+public Employee(int id, String name, Address address) {  
+    super();  
     this.id = id;  
     this.name = name;  
+    this.address = address;  
 }  
+  
 
 void show(){  
     System.out.println(id+" "+name);  
+    System.out.println(address.toString());  
 }  
 
 }   
